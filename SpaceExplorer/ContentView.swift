97f128665db897f4d45c8b48ae4fbe991f0afc65
Lukas Haring <lukasharing123@gmail.com>
2023-05-08 09:45:33 +0200

    Added new object picker, plane finder and loading screen

--- SpaceExplorer/ContentView.swift
+++ SpaceExplorer/ContentView.swift
@@ -2,33 +2,66 @@ import SwiftUI
 import RealityKit
 import ARKit
 
-struct ContentView : View {
+struct ContentView: View {
     @State private var selectedElement = 0
-    @State private var selectedElementModel = "Animated_Moon.usdz"
-    @State private var isSoundEnabled = true
+    @State private var selectedElementModel = "empty"
+    @State private var isLoading = true
     
-    let spaceModelOptions = ["Pick an item to view", "Cosmonaut suit", "Lunar rover US", "Jamestown lunar base (habitat)"]
-    let spaceModels = ["Animated_Moon.usdz", "CosmonautSuit_en.reality", "LunarRover_English.reality", "hab_en.reality"]
+    let spaceModelOptions = [ "Cosmonaut suit", "Lunar rover US", "Jamestown base"]
+    let spaceModels = ["CosmonautSuit_en.reality", "LunarRover_English.reality", "hab_en.reality"]
+    let imageNames = ["cosmonaut", "rover", "base"]
     
     var body: some View {
-        ZStack {
-            ARViewContainer(elementModel: $selectedElementModel)
-                .edgesIgnoringSafeArea(.all) // Make the camera view fullscreen
-            
-            VStack {
-                Spacer()
-                Picker(selection: $selectedElement, label: Text("Select Element")) {
-                    ForEach(spaceModelOptions.indices, id: \.self) { index in
-                        Text(spaceModelOptions[index]).tag(index)
-                    }
-                }
-                .pickerStyle(WheelPickerStyle())
-                .background(Color.black.opacity(0.8)) // Change to dark mode
-                .frame(height: 100) // Adjust the height of the picker
-                .padding(.horizontal, 0) // Set horizontal padding to 0
-                .edgesIgnoringSafeArea(.bottom) // Ignore safe area at the bottom
-                .onChange(of: selectedElement) { newValue in
-                    selectedElementModel = spaceModels[newValue]
+        if isLoading {
+           VStack {
+               Text("Space Explorer")
+                   .font(.system(size: 30))
+                   .fontWeight(.bold)
+                   .foregroundColor(.white)
+               Text("Loading...")
+                   .font(.system(size: 15))
+                   .foregroundColor(.white)
+           }
+           .frame(maxWidth: .infinity, maxHeight: .infinity)
+           .background(Color.black.opacity(0.8))
+           .edgesIgnoringSafeArea(.all)
+           .onAppear {
+               DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                   isLoading = false
+               }
+           }
+        } else {
+            ZStack {
+                ARViewContainer(elementModel: $selectedElementModel)
+                    .edgesIgnoringSafeArea(.all) // Make the camera view fullscreen
+                
+                VStack {
+                    Spacer()
+                    Text("Pick a model")
+                        .font(.system(size: 15))
+                        .foregroundColor(.white)
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 20) {
+                                ForEach(spaceModelOptions.indices, id: \.self) { index in
+                                    VStack {
+                                        Button(action: {
+                                            selectedElement = index
+                                            selectedElementModel = spaceModels[index]
+                                        }) {
+                                            Image(imageNames[index])
+                                                .resizable()
+                                                .frame(width: 100, height: 100)
+                                                .aspectRatio(contentMode: .fit)
+                                        }
+                                        Text(spaceModelOptions[index])
+                                            .font(.caption)
+                                            .foregroundColor(.white)
+                                    }
+                                }
+                            }
+                            .padding(.horizontal, (26))
+                        }
+                    .frame(height: 140)
                 }
             }
         }
@@ -40,13 +73,14 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
+        
+        // Enable horizontal plane detection
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        arView.session.run(configuration)
+        
         arView.session.delegate = context.coordinator
         context.coordinator.arView = arView
-        
-        // Add this code block to set the initial object
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            context.coordinator.selectedModel = elementModel
-        }
         
         return arView
     }
@@ -62,33 +96,64 @@ struct ARViewContainer: UIViewRepresentable {
     class Coordinator: NSObject, ARSessionDelegate {
         var parent: ARViewContainer
         var arView: ARView?
-        
+
         var selectedModel: String? {
             didSet {
                 guard let arView = arView else { return }
                 arView.scene.anchors.removeAll()
-                
+
                 if let modelName = selectedModel, let modelEntity = try? Entity.load(named: modelName) {
+                    // Apply scale to the modelEntity
+                    modelEntity.scale = SIMD3<Float>(5, 5, 5)
+
                     // Get the camera transform
-                    guard let cameraTransform = arView.session.currentFrame?.camera.transform else {
+                    guard (arView.session.currentFrame?.camera.transform) != nil else {
                         return
                     }
-                    
-                    // Create a translation relative to the camera position
-                    let translation = simd_make_float4(0, 0, -0.5, 1)
-                    let position = simd_mul(cameraTransform, translation)
-                    
-                    // Create an AnchorEntity at the new position
-                    let anchor = AnchorEntity(world: SIMD3<Float>(position.x, position.y, position.z))
-                    anchor.addChild(modelEntity)
-                    arView.scene.addAnchor(anchor)
+
+                    // Perform a raycast to find the floor
+                    let query = ARRaycastQuery(origin: arView.cameraTransform.translation,
+                                               direction: SIMD3<Float>(-arView.cameraTransform.matrix.columns.2.x, -arView.cameraTransform.matrix.columns.2.y, -arView.cameraTransform.matrix.columns.2.z),
+                                               allowing: .estimatedPlane,
+                                               alignment: .horizontal)
+                    let raycastResults = arView.session.raycast(query)
+
+                    if let result = raycastResults.first {
+                        // Create an AnchorEntity at the raycast intersection with the floor
+                        let anchor = AnchorEntity(world: SIMD3<Float>(result.worldTransform.columns.3.x, result.worldTransform.columns.3.y, result.worldTransform.columns.3.z))
+                        anchor.addChild(modelEntity)
+                        arView.scene.addAnchor(anchor)
+                    }
                 }
             }
         }
-        
+
         init(_ parent: ARViewContainer) {
             self.parent = parent
             super.init()
+        }
+
+        func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+           guard let arView = arView, let modelName = selectedModel, let modelEntity = try? Entity.load(named: modelName) else { return }
+
+           if arView.scene.anchors.count == 0 {
+               // Check if the added anchor is an ARPlaneAnchor
+               for anchor in anchors {
+                   if let planeAnchor = anchor as? ARPlaneAnchor {
+                       // Create an AnchorEntity at the plane's position
+                       let anchorEntity = AnchorEntity(anchor: planeAnchor)
+                       anchorEntity.addChild(modelEntity)
+                       arView.scene.addAnchor(anchorEntity)
+
+                       // Stop plane detection and updating the session after placing the model
+                       let configuration = ARWorldTrackingConfiguration()
+                       configuration.planeDetection = []
+                       arView.session.run(configuration, options: [])
+
+                       break
+                   }
+               }
+           }
         }
     }
 }
